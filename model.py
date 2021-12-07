4c7108b6cd8eabf92a67aeeba98de6f5ab4f9809
wancc3 <47017305+wancc3@users.noreply.github.com>
2021-12-07 00:14:55 -0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,338 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from module import *
+
+
+class Generator32(nn.Module):
+    r"""
+    ResNet backbone generator for SNGAN.
+    Attributes:
+        nz (int): Noise dimension for upsampling.
+        ngf (int): Variable controlling generator feature map sizes.
+        bottom_width (int): Starting width for upsampling generator output to an image.
+    """
+
+    def __init__(self, nz=128, ngf=256, bottom_width=4):
+        super().__init__()
+
+        self.l1 = nn.Linear(nz, (bottom_width ** 2) * ngf)
+        self.unfatten = nn.Unflatten(1, (ngf, bottom_width, bottom_width))
+        self.block2 = GBlock(ngf, ngf, upsample=True)
+        self.block3 = GBlock(ngf, ngf, upsample=True)
+        self.block4 = GBlock(ngf, ngf, upsample=True)
+        self.b5 = nn.BatchNorm2d(ngf)
+        self.c5 = nn.Conv2d(ngf, 3, 3, 1, padding=1)
+        self.activation = nn.ReLU(True)
+
+        nn.init.xavier_uniform_(self.l1.weight.data, 1.0)
+        nn.init.xavier_uniform_(self.c5.weight.data, 1.0)
+
+    def forward(self, x):
+        h = self.l1(x)
+        h = self.unfatten(h)
+        h = self.block2(h)
+        h = self.block3(h)
+        h = self.block4(h)
+        h = self.b5(h)
+        h = self.activation(h)
+        h = self.c5(h)
+        y = torch.tanh(h)
+        return y
+
+
+class Discriminator32(nn.Module):
+    r"""
+    ResNet backbone discriminator for SNGAN.
+    Attributes:
+        ndf (int): Variable controlling discriminator feature map sizes.
+    """
+
+    def __init__(self, ndf=128):
+        super().__init__()
+
+        self.block1 = DBlockOptimized(3, ndf)
+        self.block2 = DBlock(ndf, ndf, downsample=True)
+        self.block3 = DBlock(ndf, ndf, downsample=False)
+        self.block4 = DBlock(ndf, ndf, downsample=False)
+        self.l5 = SNLinear(ndf, 1)
+        self.activation = nn.ReLU(True)
+
+        nn.init.xavier_uniform_(self.l5.weight.data, 1.0)
+
+    def forward(self, x):
+        h = x
+        h = self.block1(h)
+        h = self.block2(h)
+        h = self.block3(h)
+        h = self.block4(h)
+        h = self.activation(h)
+        h = torch.sum(h, dim=(2, 3))
+        y = self.l5(h)
+        return y
+
+
+'''class Generator64(nn.Module):
+    r"""
+    ResNet backbone generator for SNGAN.
+    Attributes:
+        nz (int): Noise dimension for upsampling.
+        ngf (int): Variable controlling generator feature map sizes.
+        bottom_width (int): Starting width for upsampling generator output to an image.
+    """
+
+    def __init__(self, nz=128, ngf=1024, bottom_width=4):
+        super().__init__()
+
+        self.l1 = nn.Linear(nz, (bottom_width ** 2) * ngf)
+        self.unfatten = nn.Unflatten(1, (ngf, bottom_width, bottom_width))
+        self.block2 = GBlock(ngf, ngf >> 1, upsample=True)
+        self.block3 = GBlock(ngf >> 1, ngf >> 2, upsample=True)
+        self.block4 = GBlock(ngf >> 2, ngf >> 3, upsample=True)
+        self.block5 = GBlock(ngf >> 3, ngf >> 4, upsample=True)
+        self.b6 = nn.BatchNorm2d(ngf >> 4)
+        self.c6 = nn.Conv2d(ngf >> 4, 3, 3, 1, padding=1)
+        self.activation = nn.ReLU(True)
+
+        nn.init.xavier_uniform_(self.l1.weight.data, 1.0)
+        nn.init.xavier_uniform_(self.c6.weight.data, 1.0)
+
+    def forward(self, x):
+        h = self.l1(x)
+        h = self.unfatten(h)
+        h = self.block2(h)
+        h = self.block3(h)
+        h = self.block4(h)
+        h = self.block5(h)
+        h = self.b6(h)
+        h = self.activation(h)
+        h = self.c6(h)
+        y = torch.tanh(h)
+        return y
+
+
+class Discriminator64(nn.Module):
+    r"""
+    ResNet backbone discriminator for SNGAN.
+    Attributes:
+        ndf (int): Variable controlling discriminator feature map sizes.
+    """
+
+    def __init__(self, ndf=1024):
+        super().__init__()
+
+        self.block1 = DBlockOptimized(3, ndf >> 4)
+        self.block2 = DBlock(ndf >> 4, ndf >> 3, downsample=True)
+        self.block3 = DBlock(ndf >> 3, ndf >> 2, downsample=True)
+        self.block4 = DBlock(ndf >> 2, ndf >> 1, downsample=True)
+        self.block5 = DBlock(ndf >> 1, ndf, downsample=True)
+        self.l6 = SNLinear(ndf, 1)
+        self.activation = nn.ReLU(True)
+
+        nn.init.xavier_uniform_(self.l6.weight.data, 1.0)
+
+    def forward(self, x):
+        h = x
+        h = self.block1(h)
+        h = self.block2(h)
+        h = self.block3(h)
+        h = self.block4(h)
+        h = self.block5(h)
+        h = self.activation(h)
+        h = torch.sum(h, dim=(2, 3))
+        y = self.l6(h)
+        return y'''
+
+
+class Generator64(nn.Module):
+    r"""
+    ResNet backbone generator for SNGAN.
+    Attributes:
+        nz (int): Noise dimension for upsampling.
+        ngf (int): Variable controlling generator feature map sizes.
+        bottom_width (int): Starting width for upsampling generator output to an image.
+    """
+
+    def __init__(self, nz=128, ngf=1024, bottom_width=4):
+        super().__init__()
+
+        self.l1 = nn.Linear(nz, (bottom_width ** 2) * ngf)
+        self.unfatten = nn.Unflatten(1, (ngf, bottom_width, bottom_width))
+        self.block2 = GBlock(ngf, ngf >> 1, upsample=True)
+        self.block3 = GBlock(ngf >> 1, ngf >> 2, upsample=True)
+        self.block4 = GBlock(ngf >> 2, ngf >> 3, upsample=True)
+        self.block5 = GBlock(ngf >> 3, ngf >> 4, upsample=True)
+        self.b6 = nn.BatchNorm2d(ngf >> 4)
+        self.c6 = nn.Conv2d(ngf >> 4, 3, 3, 1, padding=1)
+        self.activation = nn.ReLU(True)
+
+        self.attn2 = Self_Attn(ngf >> 1)
+        self.attn3 = Self_Attn(ngf >> 2)
+        self.attn4 = Self_Attn(ngf >> 3)
+        self.attn5 = Self_Attn(ngf >> 4)
+
+        nn.init.xavier_uniform_(self.l1.weight.data, 1.0)
+        nn.init.xavier_uniform_(self.c6.weight.data, 1.0)
+
+    def forward(self, x):
+        h = self.l1(x)
+        h = self.unfatten(h)
+        h = self.block2(h)
+        h, _ = self.attn2(h)
+        h = self.block3(h)
+        #h, _ = self.attn3(h)
+        h = self.block4(h)
+        #h, _ = self.attn4(h)
+        h = self.block5(h)
+        #h, _ = self.attn5(h)
+        h = self.b6(h)
+        h = self.activation(h)
+        h = self.c6(h)
+        y = torch.tanh(h)
+        return y
+
+
+class Discriminator64(nn.Module):
+    r"""
+    ResNet backbone discriminator for SNGAN.
+    Attributes:
+        ndf (int): Variable controlling discriminator feature map sizes.
+    """
+
+    def __init__(self, ndf=1024):
+        super().__init__()
+
+        self.block1 = DBlockOptimized(3, ndf >> 4)
+        self.block2 = DBlock(ndf >> 4, ndf >> 3, downsample=True)
+        self.block3 = DBlock(ndf >> 3, ndf >> 2, downsample=True)
+        self.block4 = DBlock(ndf >> 2, ndf >> 1, downsample=True)
+        self.block5 = DBlock(ndf >> 1, ndf, downsample=True)
+        self.l6 = SNLinear(ndf, 1)
+        self.activation = nn.ReLU(True)
+
+        self.attn1 = Self_Attn(ndf >> 4)
+        self.attn2 = Self_Attn(ndf >> 3)
+        self.attn3 = Self_Attn(ndf >> 2)
+        self.attn4 = Self_Attn(ndf >> 1)
+
+        nn.init.xavier_uniform_(self.l6.weight.data, 1.0)
+
+    def forward(self, x):
+        h = x
+        h = self.block1(h)
+        #h, _ = self.attn1(h)
+        h = self.block2(h)
+        #h, _ = self.attn2(h)
+        h = self.block3(h)
+        #h, _ = self.attn3(h)
+        h = self.block4(h)
+        #h, _ = self.attn4(h)
+        h = self.block5(h)
+        h = self.activation(h)
+        h = torch.sum(h, dim=(2, 3))
+        y = self.l6(h)
+        return y
+    
+class Generator128(nn.Module):
+    r"""
+    ResNet backbone generator for SNGAN.
+    Attributes:
+        nz (int): Noise dimension for upsampling.
+        ngf (int): Variable controlling generator feature map sizes.
+        bottom_width (int): Starting width for upsampling generator output to an image.
+    """
+
+    def __init__(self, nz=128, ngf=2048, bottom_width=4):
+        super().__init__()
+
+        self.l1 = nn.Linear(nz, (bottom_width ** 2) * ngf)
+        self.unfatten = nn.Unflatten(1, (ngf, bottom_width, bottom_width))
+        self.block2 = GBlock(ngf, ngf >> 1, upsample=True)
+        self.block3 = GBlock(ngf >> 1, ngf >> 2, upsample=True)
+        self.block4 = GBlock(ngf >> 2, ngf >> 3, upsample=True)
+        self.block5 = GBlock(ngf >> 3, ngf >> 4, upsample=True)
+        self.block6 = GBlock(ngf >> 4, ngf >> 5, upsample=True)
+        self.b6 = nn.BatchNorm2d(ngf >> 5)
+        self.c6 = nn.Conv2d(ngf >> 5, 3, 3, 1, padding=1)
+        self.activation = nn.ReLU(True)
+
+        self.attn2 = Self_Attn(ngf >> 1)
+        self.attn3 = Self_Attn(ngf >> 2)
+        self.attn4 = Self_Attn(ngf >> 3)
+        self.attn5 = Self_Attn(ngf >> 4)
+        self.attn6 = Self_Attn(ngf >> 5)
+
+        nn.init.xavier_uniform_(self.l1.weight.data, 1.0)
+        nn.init.xavier_uniform_(self.c6.weight.data, 1.0)
+
+    def forward(self, x):
+        h = self.l1(x)
+        h = self.unfatten(h)
+        h = self.block2(h)
+        #h, _ = self.attn2(h)
+        h = self.block3(h)
+        #h, _ = self.attn3(h)
+        h = self.block4(h)
+        #h, _ = self.attn4(h)
+        h = self.block5(h)
+        #h, _ = self.attn5(h)
+        h = self.block6(h)
+        #h, _ = self.attn6(h)
+        h = self.b6(h)
+        h = self.activation(h)
+        h = self.c6(h)
+        y = torch.tanh(h)
+        return y
+
+
+class Discriminator128(nn.Module):
+    r"""
+    ResNet backbone discriminator for SNGAN.
+    Attributes:
+        ndf (int): Variable controlling discriminator feature map sizes.
+    """
+
+    def __init__(self, ndf=2048):
+        super().__init__()
+
+        self.block0 = DBlockOptimized(3, ndf >> 5)
+        self.block1 = DBlock(ndf >> 5, ndf >> 4, downsample=True)
+        self.block2 = DBlock(ndf >> 4, ndf >> 3, downsample=True)
+        self.block3 = DBlock(ndf >> 3, ndf >> 2, downsample=True)
+        self.block4 = DBlock(ndf >> 2, ndf >> 1, downsample=True)
+        self.block5 = DBlock(ndf >> 1, ndf, downsample=True)
+        self.l6 = SNLinear(ndf, 1)
+        self.activation = nn.ReLU(True)
+
+        self.attn0 = Self_Attn(ndf >> 5)
+        self.attn1 = Self_Attn(ndf >> 4)
+        self.attn2 = Self_Attn(ndf >> 3)
+        self.attn3 = Self_Attn(ndf >> 2)
+        self.attn4 = Self_Attn(ndf >> 1)
+
+        nn.init.xavier_uniform_(self.l6.weight.data, 1.0)
+
+    def forward(self, x):
+        h = x
+        h = self.block0(h)
+        #h, _ = self.attn0(h)
+        h = self.block1(h)
+        #h, _ = self.attn1(h)
+        h = self.block2(h)
+        #h, _ = self.attn2(h)
+        h = self.block3(h)
+        #h, _ = self.attn3(h)
+        h = self.block4(h)
+        #h, _ = self.attn4(h)
+        h = self.block5(h)
+        h = self.activation(h)
+        h = torch.sum(h, dim=(2, 3))
+        y = self.l6(h)
+        return y
+
+
+
+
